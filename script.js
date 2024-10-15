a1c9d624d6a76fafcd652b3f6977677c1d4c6bcb
victoriarahme <vic.rahme@gmail.com>
2024-10-15 19:26:50 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.querySelectorAll
+
+ document.getElementById('save-btn').addEventListener('click', function() {
+    const userInput = documentElementById('nameInput').value;
+    localStorage.setItem('saveButton', userInput);
+
+ });
+
+ //When the page is loaded or refreshed, check if a name is saved in localStorage and display it.
+
+ localStorage.getItem(#username);
+
+
+const clearButton = document.body.querySelector("#clear-btn");
+clearButton.addEventListener("click", () => {
+  clearButton.textContent = "";
+});
+
+ 
+ 
+
+
+
